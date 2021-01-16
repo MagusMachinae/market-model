21acfbc9e272254a6ccdbb82561829ddf380cf87
MagusMachinae <Edward.Hughes1911@gmail.com>
2021-01-16 15:49:20 +0000

    changed project to init in tree-parser

--- project.clj
+++ project.clj
@@ -4,9 +4,9 @@
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
   :dependencies [[org.clojure/clojure "1.10.1"]
-                 
+
                  [clojupyter "0.3.2"]
                  [clj-python/libpython-clj "2.00-alpha-7"]
                  [cnuernber/dtype-next "6.00-beta-20"]]
 
-  :repl-options {:init-ns market-model.core})
+  :repl-options {:init-ns market-model.tree-parser})
