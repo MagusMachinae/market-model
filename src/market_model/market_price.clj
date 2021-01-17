8c1cec9e9112d977ee51eaf307c3880aac3893cc
MagusMachinae <Edward.Hughes1911@gmail.com>
2021-01-17 15:50:06 +0000

    Added require to grab functions in trees.clj

--- src/market_model/market_price.clj
+++ src/market_model/market_price.clj
@@ -1,5 +1,7 @@
 (ns market-model.market-price
   (:require [clojure.core.reducers :as r]
-            [market-model.trees :as trees-ns]))
+            [market-model.trees :as trees-ns]
+            [clojure.repl :as repl]))
 
+(repl/dir trees-ns)
 (defn derive-market-price [{}])
