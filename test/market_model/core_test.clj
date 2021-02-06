e98d2869fc9d6b4fd38d88e6d3365ee1d951cfbb
MagusMachinae <Edward.Hughes1911@gmail.com>
2021-02-06 19:35:37 +0000

    Test values loaded in test-ns

--- test/market_model/core_test.clj
+++ test/market_model/core_test.clj
@@ -1,6 +1,12 @@
 (ns market-model.core-test
   (:require [clojure.test :refer :all]
-            [market-model.core :refer :all]))
+            ;[market-model.market-price :refer :all]
+            [clojure-csv.core :as csv]))
+
+(def test-battery (map rest (csv/parse-csv (slurp "ext/validate_output.csv"))))
+(def feature-names (take 13 (first test-battery)))
+(def inputs (rest (map (fn [x] (take 13 x)) test-battery)))
+(def expected-results (map last (rest test-battery)))
 
 (deftest a-test
   (testing "FIXME, I fail."
