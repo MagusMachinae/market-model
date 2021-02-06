6ac87728caa2ca629304c67d0eaa4f9a2f0834ad
MagusMachinae <Edward.Hughes1911@gmail.com>
2021-02-06 18:25:52 +0000

    Added csv parser

--- project.clj
+++ project.clj
@@ -4,7 +4,7 @@
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
   :dependencies [[org.clojure/clojure "1.10.1"]
-
+                 [clojure-csv/clojure-csv "2.0.1"]
                  [clojupyter "0.3.2"]
                  [clj-python/libpython-clj "2.00-alpha-7"]
                  [cnuernber/dtype-next "6.00-beta-20"]]
