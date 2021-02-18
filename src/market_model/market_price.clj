f60be481f8ec841bc4d85bc6637cc29abcf188c6
MagusMachinae <Edward.Hughes1911@gmail.com>
2021-02-18 16:26:23 +0000

    Included vars defined in test-ns for prototyping

--- src/market_model/market_price.clj
+++ src/market_model/market_price.clj
@@ -1,7 +1,8 @@
 (ns market-model.market-price
   (:require [clojure.core.reducers :as r]
             [trees :as trees-ns]
-            [clojure.repl :as repl]))
+            [clojure.repl :as repl]
+            [market-model.core-test :as t]))
 
 (repl/dir trees-ns)
 (defn derive-market-price
