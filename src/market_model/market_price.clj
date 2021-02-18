0206e6682e813ce282818a6e394e3c8e02656d1a
MagusMachinae <Edward.Hughes1911@gmail.com>
2021-02-18 16:28:52 +0000

    Comparing fold with other reductions to explore complexity space of fn

--- src/market_model/market_price.clj
+++ src/market_model/market_price.clj
@@ -6,7 +6,20 @@
 
 
 (defn derive-market-price
-  "Runs regression models over input."
-  [{}]
-  (->>))
+  "Runs regression models over input. Takes a filepath of the location of trees.edn"
+  [path [data-set]]
+  (r/fold + (r/map (fn [f] (f data-set)))))
 (keys (ns-publics 'trees))
+
+
+
+(time (r/fold + (r/map (fn [x] (* 2 x)) (into [] (range 100000)))))
+(time (reduce + (map (fn [x] (* 2 x)) (into [] (range 100000)))))
+(time (reduce + (map (fn [f] (apply (eval  f) (into [] (first t/inputs)))) (read-string (str "[" (slurp "trees.edn") "]")))))
+(def func (first (read-string (str "[" (slurp "trees.edn") "]"))))
+(class (fn [x] (+ x x)))
+(eval func)
+(apply (partial func) (first t/inputs))
+(apply (first (read-string (str "[" (slurp "trees.edn") "]"))) (first t/inputs))
+
+((fn [foo] (+ 2 foo)) 1 2 3 4)
