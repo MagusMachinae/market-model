ab86b0408dcac98bde7535f2c26a7e4d01f4e35f
MagusMachinae <Edward.Hughes1911@gmail.com>
2021-02-19 19:26:06 +0000

    removed reference to trees-ns

--- src/market_model/market_price.clj
+++ src/market_model/market_price.clj
@@ -1,6 +1,5 @@
 (ns market-model.market-price
   (:require [clojure.core.reducers :as r]
-            [trees :as trees-ns]
             [clojure.repl :as repl]
             [market-model.core-test :as t]))
 
@@ -15,11 +14,14 @@
 
 (time (r/fold + (r/map (fn [x] (* 2 x)) (into [] (range 100000)))))
 (time (reduce + (map (fn [x] (* 2 x)) (into [] (range 100000)))))
-(time (reduce + (map (fn [f] (apply (eval  f) (into [] (first t/inputs)))) (read-string (str "[" (slurp "trees.edn") "]")))))
+(time (reduce +
+              (map
+               (fn [f] (apply (eval f) (into [] (first t/inputs))))
+               (read-string (str "[" (slurp "trees.edn") "]")))))
 (def func (first (read-string (str "[" (slurp "trees.edn") "]"))))
 (class (fn [x] (+ x x)))
 (eval func)
-(apply (partial func) (first t/inputs))
+
 (apply (first (read-string (str "[" (slurp "trees.edn") "]"))) (first t/inputs))
 
 ((fn [foo] (+ 2 foo)) 1 2 3 4)
