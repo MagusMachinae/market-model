cf9de9cc76cdfe2e4646d554dcd6f49b3ed9ed0c
MagusMachinae <Edward.Hughes1911@gmail.com>
2021-02-21 20:51:50 +0000

    pointing to file in ext/

--- src/market_model/market_price.clj
+++ src/market_model/market_price.clj
@@ -3,7 +3,7 @@
             [clojure.repl :as repl]
             [market-model.core-test :as t]))
 
-(def funcs (read-string (slurp "trees.edn")))
+(def funcs (read-string (slurp "ext/trees.edn")))
 
 (defn derive-market-price
   "Runs regression models over input. Takes a filepath of the location of trees.edn"
@@ -14,32 +14,32 @@
 
 
 (time (doall  (pmap (fn [args] (r/fold + (r/map (fn [f] (apply (eval f) args))
-                                                (read-string (slurp "trees.edn") ))))
+                                                (read-string (slurp "ext/trees.edn") ))))
                     (pmap (fn [coll] (into [] coll)) t/inputs))))
 
 (into [] (r/map (fn [f] (apply (eval f) (into [] (first t/inputs))))
                 (read-string (str "[" (slurp "ext/trees.edn") "]"))))
 
 (time (r/fold + (r/map (fn [f] (apply (eval f) (into [] (first t/inputs))))
-                       (read-string (slurp "trees.edn") ))))
+                       (read-string (slurp "ext/trees.edn") ))))
 
 (time (reduce + (map (fn [x] (* 2 x)) (into [] (range 100000)))))
 (time (reduce +
               (map
                (fn [f] (apply (eval f) (into [] (first t/inputs))))
-               (read-string (slurp "trees.edn")))))
+               (read-string (slurp "ext/trees.edn")))))
 (time (r/reduce +
                           (pmap
                            (fn [f] (apply (eval f)  (first t/inputs)))
                            funcs)))
-(time (read-string (slurp "trees.edn")))
+(time (read-string (slurp "ext/trees.edn")))
 
 (time (doall (pmap
          (fn [f] (apply (eval f)  (first t/inputs)))
-         (read-string (slurp "trees.edn")))))
+         (read-string (slurp "ext/trees.edn")))))
 
-(def func (first (read-string (str "[" (slurp "trees.edn") "]"))))
+(def func (first (read-string (str "[" (slurp "ext/trees.edn") "]"))))
 (class (fn [x] (+ x x)))
 (eval func)
 
-(apply (first (read-string (str "[" (slurp "trees.edn") "]"))) (first t/inputs))
+(apply (first (read-string (str "[" (slurp "ext/trees.edn") "]"))) (first t/inputs))
