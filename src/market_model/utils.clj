0497941ccb4dae69f2b09c4496ca3a40309a40a3
MagusMachinae <Edward.Hughes1911@gmail.com>
2021-02-28 21:41:42 +0000

    implemented furthest-value-from

--- src/market_model/utils.clj
+++ src/market_model/utils.clj
@@ -1,14 +1,11 @@
 (ns market-model.utils
-  (:require [tick.alpha.api :as tick]))
+  (:require [tick.alpha.api :as tick]
+            [clojure.math.numeric-tower :as math]))
 
 (defn time-since)
 
 (defn furthest-value-from
-  ([val-a val-b]
-   (if (> val-a val-b)
-     val-a
-     val-b))
-  ([val-a val-b offset]
-    (if (apply > (map (fn [x] (- x offset)) '(val-a val-b)))
-      val-a
-      val-b)))
+  [val-a val-b fixed-point]
+  (if (apply > (map (fn [x] (math/abs (- x fixed-point))) '(val-a val-b)))
+    val-a
+    val-b))
