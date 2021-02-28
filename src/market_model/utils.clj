7c6a0583ca93e0776d6be5772406f1cad7c703e5
MagusMachinae <Edward.Hughes1911@gmail.com>
2021-02-28 21:59:52 +0000

    multiarity defn for furthest-value-from

--- src/market_model/utils.clj
+++ src/market_model/utils.clj
@@ -2,10 +2,20 @@
   (:require [tick.alpha.api :as tick]
             [clojure.math.numeric-tower :as math]))
 
-(defn time-since)
+(defn time-since
+  [])
 
 (defn furthest-value-from
-  [val-a val-b fixed-point]
-  (if (apply > (map (fn [x] (math/abs (- x fixed-point))) '(val-a val-b)))
-    val-a
-    val-b))
+  ([fixed-point val-a val-b]
+   (if (apply > (map (fn [x] (math/abs (- x fixed-point))) '(val-a val-b)))
+     val-a
+     val-b))
+  ([fixed-point val-a val-b & values]
+   (loop [arg-1 val-a
+          arg-2 val-b
+          stack values]
+     (if (next stack)
+       (recur (furthest-value-from fixed-point arg-1 arg-2)
+              (first stack)
+              (rest stack))
+       (furthest-value-from fixed-point arg-1 arg-2)))))
