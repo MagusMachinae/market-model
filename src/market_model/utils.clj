726451525936473bf588a7aab58a5ff2a0ad1d48
MagusMachinae <Edward.Hughes1911@gmail.com>
2021-03-01 00:07:06 +0000

    fixed bug in arbitrary arity implementation of furthest-value-from

--- src/market_model/utils.clj
+++ src/market_model/utils.clj
@@ -3,19 +3,31 @@
             [clojure.math.numeric-tower :as math]))
 
 (defn time-since
-  [])
+  [date options])
+
+(tick/divide )
+(tick/new-interval (tick/now) (tick/<< (tick/now) (tick/new-period 1 :years)))
+(tick/year 2020)
 
 (defn furthest-value-from
   ([fixed-point val-a val-b]
-   (if (apply > (map (fn [x] (math/abs (- x fixed-point))) '(val-a val-b)))
+   (if (apply > (map (fn [x] (math/abs (- x fixed-point))) [val-a val-b]))
      val-a
      val-b))
   ([fixed-point val-a val-b & values]
    (loop [arg-1 val-a
           arg-2 val-b
           stack values]
-     (if (next stack)
+     (if (seq stack)
        (recur (furthest-value-from fixed-point arg-1 arg-2)
               (first stack)
               (rest stack))
        (furthest-value-from fixed-point arg-1 arg-2)))))
+
+(furthest-value-from 2 5 -10 -2 -100)
+(seq '())
+(if nil
+  1
+  2)
+(map (fn [x] (math/abs (- x 2))) '(5 -10))
+(apply > (map (fn [x] (math/abs (- x 2))) '(5 -10)))
